733f3b4ba96dc91d3675117eb5b22d3924580934
Maucoweb <maucotel.pro@gmail.com>
2022-08-10 18:11:16 +0200

    fiche produit img ok

--- front/js/product.js
+++ front/js/product.js
@@ -1,48 +1,40 @@
-(async function () { 
-    const itemId = getItemId ()
+(async function () {
+    const itemId = getItemId()
     console.log(itemId)
-    const item = await getItem (itemId)
-    displayId (itemId)
-    
-    }) ()
-    
+    displayItem(await getItem(itemId));
 
-    
-    function getItemId () {
-        
-        return new URL (location.href).searchParams.get("id");
-    }
-
-    function getItem (itemId) {
-        
-return fetch(`http://localhost:3000/api/products/${itemId}`)
-    .then(itemId => {
-        console.log(itemId);
-        return itemId.json();
-        
-    })
-    
-    .then(itemId => {
-        console.log(itemId);
-      itemId.for(function(displayId) {
-    itemId(displayId);
+})()
 
 
-      })
-        
 
+function getItemId() {
+
+    return new URL(location.href).searchParams.get("id");
+}
+
+function getItem(itemId) {
+
+    return fetch(`http://localhost:3000/api/products/${itemId}`)
+        .then(itemId => {
+            console.log(itemId);
+            return itemId.json()
 
         })
 
-        
-    }
 
-  
+}
 
-   
-        
+function displayItem(kanap) {
+    var img = document.createElement("img");
+    img.setAttribute("src", kanap.imageUrl);
+    var imageContainer = document.querySelector(".item__img");
+
+    imageContainer.appendChild(img);
+}
 
 
 
-   
-    
+
+
+
+
